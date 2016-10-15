461678d0c82eb056002ddc004860bcc8ddd528c2
squireChuck <squirechucks.codequest@gmail.com>
2016-10-15 15:19:33 -0400

    adding name and abbrev endpoints

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,8 +1,5 @@
 /*
-    REST ideas
-    GET /states => all state obj's
-    GET /states/name/:name => info on state with name
-    GET /states/abbrev/:abbrev => info on state with abbrev
+    Future ideas
     GET /states/(name|abbrev)/(:name|:abbrev)/(capital|addresses|driversLicenseDescription|sampleLicense)    addresses => all addresses
     GET /states/(name|abbrev)/(:name|:abbrev)/addresses => all addresses
     GET /states/(name|abbrev)/(:name|:abbrev)/addresses/:index => single address
@@ -12,33 +9,24 @@ var stateService = new StateService();
 
 module.exports = function(app) {
 
-    // GET endpoint
+    // GET /states
     app.get('/app/api/v1/states', function(req, res) { 
-        // This log shows up in the console running node (since this is server side code).
         console.log("In the GET /app/api/v1/states endpoint");
 
-        // Return json in the response.
         res.send(stateService.getAllStates());
     });
 
-    // Second GET endpoint
-    app.get('/app/api/v1/text', function(req, res) { 
-        res.send({"textMessage": "you rock! :D :D :D"});
+    // GET /states/name 
+    app.get('/app/api/v1/states/name/:name', function(req, res) { 
+        console.log("In the GET /app/api/v1/states/name endpoint");
+
+        res.send(stateService.getStateByName(req.params.name));
     });
 
-    // POST endpoint
-    app.post('/app/api/v1', function(req, res) { 
-        var reqData = req.body;
-        // This log shows up in the console running node (since this is server side code).
-        console.log(JSON.stringify(reqData));
-        
-        var name = req.body.name;
-        var myCount = parseInt(req.body.count) + 11;
+    // GET /states/abbrev 
+    app.get('/app/api/v1/states/abbrev/:abbrev', function(req, res) { 
+        console.log("In the GET /app/api/v1/states/abbrev endpoint");
 
-        res.send({
-            'name' : name, // Should be the same as what was in the POST request.
-            // Should be different than the POST request.
-            'myCount': myCount 
-        });
+        res.send(stateService.getStateByAbbrev(req.params.abbrev));
     });
 }
